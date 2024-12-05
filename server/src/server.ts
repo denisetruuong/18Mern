d9030c7b1396122504fa5d5b2e438ddabd458887
denisetruuong <denise.truong1124@gmail.com>
2024-12-05 11:16:34 -0600

    more server ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,11 @@ app.use(express.json());
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
